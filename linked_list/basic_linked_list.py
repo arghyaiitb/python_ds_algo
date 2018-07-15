e0476e94b1ecfbc3807968deb77f055c9ef22e7c
Arghya Saha <arghyaiitb>
2018-07-15 16:28:41 +0530

    basic linked list in python

---
+++ linked_list/basic_linked_list.py
@@ -0,0 +1,62 @@
+class Node:
+    def __init__(self, data= None):
+        self.data = data
+        self.next = None
+
+
+class LinkedList:
+    def __init__(self):
+        self.head = Node()
+
+    def append(self, data):
+        new_node = Node(data)
+        current_node = self.head
+        while current_node.next != None:
+            current_node = current_node.next
+        current_node.next = new_node
+
+    def length(self):
+        current_node = self.head
+        total = 0
+        while current_node.next != None:
+            current_node = current_node.next
+            total += 1
+        return total
+
+    def display(self):
+        elements = []
+        current_node = self.head
+        while current_node.next != None:
+            current_node = current_node.next
+            elements.append(current_node.data)
+        print(elements)
+
+    def get(self, index):
+        if index>self.length():
+            print('ERROR')
+        current_index = 0
+        current_node = self.head
+        while True:
+            current_node = current_node.next
+            if current_index == index:
+                return current_node.data
+            current_index += 1
+
+    def erase(self,index):
+        if index>self.length():
+            print('ERROR')
+        current_index = 0
+        current_node = self.head
+        while True:
+            last_node = current_node
+            current_node = current_node.next
+            if current_index == index:
+                last_node.next = current_node.next
+                return
+            current_index += 1
+
+ll = LinkedList()
+print(ll.display())
+ll.append(1)
+ll.append(2)
+print(ll.display())
